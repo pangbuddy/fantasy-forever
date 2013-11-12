c586c237f4475f78c4fec08b1674949a3b3e0acc
pangbuddy@gmail.com <pangbuddy@gmail.com@c6965982-ab02-0ff6-95a1-07644ae00c23>
2013-11-12 17:09:16 +0000

    add new HabitJuge class

--- trunk/fantasy-forever/src/main/java/com/aooci/ff/juge/AbstractJuge.java
+++ trunk/fantasy-forever/src/main/java/com/aooci/ff/juge/AbstractJuge.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public abstract class AbstractJuge {
 
+	public final static float scoreStep = (float) 0.1;
+	
 	abstract Map<Integer, Float> examine(boolean[][] resultData);
 	
 }
