cad8381968db349979d2dd71b7152a95f3414ddd
pangbuddy@gmail.com <pangbuddy@gmail.com@c6965982-ab02-0ff6-95a1-07644ae00c23>
2013-11-13 17:09:33 +0000

    implemented HabitJuge

--- trunk/fantasy-forever/src/main/java/com/aooci/ff/juge/HabitJuge.java
+++ trunk/fantasy-forever/src/main/java/com/aooci/ff/juge/HabitJuge.java
@@ -1,49 +1,46 @@
 package com.aooci.ff.juge;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class HabitJuge extends AbstractJuge{
 
 	@Override
-	Map<Integer, Float> examine(boolean[][] resultData) {
-		boolean hasHabit;
-		int habitPeriod;
+	public void examine(Map<Integer, AtomicInteger> scores, boolean[][] resultData) {
+		boolean hasHabit, habitRecoded;
+		int habitPeriod, periodCounter;
 		for(int dimensionIndex=0; dimensionIndex < resultData[0].length; dimensionIndex++){
 			hasHabit = false;
+			habitRecoded = false;
 			habitPeriod = 0;
+			periodCounter = 0;
 			for(int periodIndex=resultData.length - 1; periodIndex > -1; periodIndex--){
-				
-				
+				if(hasHabit){
+					break;
+				}else if(habitRecoded){
+					if(resultData[periodIndex][dimensionIndex]){
+						if(habitPeriod == periodCounter){
+							hasHabit = true;
+						}else{
+							periodCounter = 0;
+							continue;
+						}
+					}else{
+						periodCounter++;
+					}
+				}else{
+					if(resultData[periodIndex][dimensionIndex]){
+						habitRecoded = true;
+					}else{
+						habitPeriod++;
+					}
+				}
+			}
+			if(hasHabit){
+				scores.get(dimensionIndex+1).incrementAndGet();
 			}
 		}
-		return null;
+		return;
 	}
-//    public Map<Integer,Integer> habit(int lastResultNumber){
-//        Map<Integer,Integer> habits = new TreeMap<Integer, Integer>();
-//        for(int recordIndex = 0 ; recordIndex < super.dimension ; recordIndex++){
-//            int habitTemplate = 0;
-//            boolean habitTemplateCreated = false;
-//            int habitCount = 0;
-//            int stepCounter = 0;
-//            for(int periodIndex = lastResultNumber-1 ; periodIndex >= 0 ; periodIndex--){
-//                
-//                if(super.binaryResults[periodIndex][recordIndex] == 1){
-//                    if(!habitTemplateCreated){
-//                        habitTemplateCreated = true;
-//                    }else if(habitTemplate == stepCounter){
-//                        habitCount++;
-//                    }
-//                    stepCounter = 0;
-//                }else if(!habitTemplateCreated){
-//                    habitTemplate++;
-//                }else{
-//                    stepCounter++;
-//                }
-//            }
-//            habits.put(recordIndex+1, habitCount);
-//            //System.out.println((recordIndex +1) +" - Habit Template = " + habitTemplate);
-//        }
-//    
-//        return habits;
-//    }
+
 }
---
+++ trunk/fantasy-forever/src/test/java/com/aooci/ff/juge/HabitJugeTest.java
@@ -0,0 +1,12 @@
+package com.aooci.ff.juge;
+
+import org.junit.Test;
+
+public class HabitJugeTest {
+
+	@Test
+	public void testExamine() {
+		//fail("Not yet implemented");
+	}
+
+}
